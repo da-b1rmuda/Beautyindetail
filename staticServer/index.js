ea326986756c7859468154553ce6e0912698a0ca
letnull19a <alexvolkovdd@gmail.com>
2024-05-17 17:13:08 +0000

    feat(routing): исправлены роуты

--- staticServer/index.js
+++ staticServer/index.js
@@ -4,40 +4,40 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'client')))
 
-app.get('/Beauty in detail.html', async (req, res) => {
+app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'Beauty in detail.html'))
 })
-app.get('/Record.html', async (req, res) => {
+app.get('/Record', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'Record.html'))
 })
-app.get('/service1.html', async (req, res) => {
+app.get('/service1', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'service1.html'))
 })
-app.get('/service2.html', async (req, res) => {
+app.get('/service2', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'service2.html'))
 })
-app.get('/service3.html', async (req, res) => {
+app.get('/service3', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'service3.html'))
 })
-app.get('/service4.html', async (req, res) => {
+app.get('/service4', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'service4.html'))
 })
-app.get('/Chart.html', async (req, res) => {
+app.get('/Chart', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'Chart.html'))
 })
-app.get('/Admin_service.html', async (req, res) => {
+app.get('/Admin_service', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'Admin_service.html'))
 })
-app.get('/Admin_record.html', async (req, res) => {
+app.get('/Admin_record', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'Admin_record.html'))
 })
-app.get('/Admin_masters.html', async (req, res) => {
+app.get('/Admin_masters', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'Admin_masters.html'))
 })
-app.get('/Admin_clients.html', async (req, res) => {
+app.get('/Admin_clients', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'Admin_clients.html'))
 })
-app.get('/Admin_categoryservice.html', async (req, res) => {
+app.get('/Admin_categoryservice', async (req, res) => {
 	res.sendFile(path.join(__dirname, 'client', 'Admin_categoryservice.html'))
 })
 
